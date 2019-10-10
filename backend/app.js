7d5cbe1b2508281cf62450529d7f587dc3108acc
aliaasayed <aliaasayed45@gmail.com>
2019-10-10 18:36:47 +0200

    connect to mongo

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,21 @@
-var express = require("express");
-var server = express();
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
 
-
-server.use(function(req,resp,next){
-  resp.header("Access-Control-Allow-Origin","*");
-  resp.header("Access-Control-Allow-Headers","Content-Type");
-  resp.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+server.use(function (req, resp, next) {
+  resp.header("Access-Control-Allow-Origin", "*");
+  resp.header("Access-Control-Allow-Headers", "Content-Type");
+  resp.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   next();
 });
 
+mongoose.connect("mongodb://localhost:27017/Zappy");
 
 
+const tweetsRouter = require('./controllers/tweeterController')
+server.use("/", tweetsRouter);
 
-server.listen(3000,function(){
+
+server.listen(3000, function () {
   console.log("Starting listen...");
 });
